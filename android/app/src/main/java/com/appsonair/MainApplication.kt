27a4d4c91571c71b285299bed7765ac8ab50474c
rohitk-iprogrammer <rohitk@iprogrammer.co>
2025-05-13 16:08:55 +0530

    apps on air implementation

--- android/app/src/main/java/com/appsonair/MainApplication.kt
+++ android/app/src/main/java/com/appsonair/MainApplication.kt
@@ -12,6 +12,9 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
+// ➕ Import CodePush
+import com.microsoft.codepush.react.CodePush
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -28,6 +31,11 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+
+        // ➕ Point to CodePush bundle in release builds
+        override fun getJSBundleFile(): String? {
+          return CodePush.getJSBundleFile()
+        }
       }
 
   override val reactHost: ReactHost
